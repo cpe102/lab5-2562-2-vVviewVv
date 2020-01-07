c45d69449d3555f59e3a6f18f72814c6616aedb3
vVviewVv <viewissara1246@gmail.com>
2020-01-07 16:09:57 +0700

    HW

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
-
-
-
+string mixText(string a,string b){
+	string y;
+	if(a.size() == b.size()){
+	int i = 0;
+	while(i < a.size()){
+		y = y+a[i]+b[i];
+		i++;
+		}
+	return y;
+	}else{
+	return "E";
+	}
+}
+	
 int main(){	
 	cout << mixText("AAA","BBB") << "\n";
 	cout << mixText("Hello","World") << "\n";
@@ -11,7 +22,9 @@ int main(){
 	cout << mixText("1234","5678") << "\n";
 	cout << mixText("6","9") << "\n";
 	cout << mixText("XXXXXXX","YYY") << "\n";	
-	cout << mixText("Y","XX") << "\n";	
+	cout << mixText("Y","XX") << "\n";
+
+
 	
 	return 0;	
 }
